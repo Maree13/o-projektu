79178507bf05d58ae1e3841e5cca71420dbf64ca
Mariana Sarovic <sarovicmariana@gmail.com>
2023-05-12 22:12:52 +0200

    Menu completed. Created menu on the rest of html pages. Created javascript function window.onload so that hover styles stay while page is loaded.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+
+// ADDING CLASS CURRENT SO THAT LINKS KEEP YELLOW COLOR WHILE ON THE PAGE
+window.onload = () => {
+  const menuLinks = document.querySelectorAll('.menu__pages a');
+  menuLinks.forEach((link) => {
+    console.log(link.pathname, window.location.pathname);
+    if (link.pathname === window.location.pathname) {
+      link.classList.add('current');
+    }
+  });
+};
